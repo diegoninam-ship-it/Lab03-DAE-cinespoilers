912f1c2cb59ebaf3eb3d51dc1888db9fc25f74af
diegoninam-ship-i <diego.nina.m@tecsup.edu.pe>
2026-04-26 09:14:25 -0500

    AddGenre

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet
+from .views import MovieViewSet, GenreViewSet  
 
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet)
+router.register(r'genres', GenreViewSet) 
 
 urlpatterns = router.urls
