fe2f8ce96d8e80730ecb2a20bc945cb32420af9a
diegoninam-ship-i <diego.nina.m@tecsup.edu.pe>
2026-04-26 09:14:25 -0500

    Funciones para  Genero

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Genre  
+
+# NUEVO
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date', 'created_at')
+    list_display = ('id', 'title', 'duration', 'is_active')
     search_fields = ('title',)
+    list_filter = ('is_active', 'genres')  
--- movies/models.py
+++ movies/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    duration = models.PositiveIntegerField(default=0)
+    duration = models.PositiveIntegerField(default=90)  # IMPORTANTE
     release_date = models.DateField()
     is_active = models.BooleanField(default=True)
+
+    genres = models.ManyToManyField(Genre, related_name='movies')
 
     created_at = models.DateTimeField(auto_now_add=True)
 
